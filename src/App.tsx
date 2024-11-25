9f0532de067dcb398a8d1571178caebfc158e189
Yash-jaiswal2509 <yashjaiswal2509@gmail.com>
2024-11-25 08:51:56 +0530

    (client): Navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import About from './components/About';
 import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar/>
       <Hero />
-
-      <section className='bg-blue-400 z-0 min-h-screen'></section>
+      <About />
     </main>
   )
 }
